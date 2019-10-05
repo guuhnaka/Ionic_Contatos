5b05c6a8d18a5ccd0f2d602006d99a873f8a801c
guuhnaka <32445083+guuhnaka@users.noreply.github.com>
2019-10-04 22:49:16 -0300

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  contatos = [
+    {
+    id: '1',
+    nome: 'Gustavin Da Leste',
+    telefone: '40028922',
+    email: 'gustavo@gmail.com'
+    },
+    {
+    id: '2',
+    nome: 'Gustavin Da Sul',
+    telefone: '40038933',
+    email: 'Nakzera@gmail.com'
+    },
+    {
+    id: '3',
+    nome: 'Gustavon Da Oeste',
+    telefone: '40048944',
+    email: 'Nakzin@gmail.com'
+    },
+    {
+    id: '4',
+    nome: 'Gustavon Do Norte',
+    telefone: '40058955',
+    email: 'NakzCorp@gmail.com'
+    }
+  ]
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onItemClick(contato){
+    console.log('item.click', contato);
   }
 
 }
